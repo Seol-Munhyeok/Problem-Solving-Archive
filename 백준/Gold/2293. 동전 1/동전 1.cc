902a839c15c34f302e5d337c0c9e42be53274be5
설문혁 <150120907+Seol-Munhyeok@users.noreply.github.com>
2026-04-23 11:11:38 +0900

    [Gold IV] Title: 동전 1, Time: 0 ms, Memory: 2060 KB -BaekjoonHub

---
+++ 백준/Gold/2293. 동전 1/동전 1.cc
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int n, k, tmp, dp[10002];
+
+int main(){
+    ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+    cin >> n >> k;
+    dp[0] = 1;
+    for (int i = 1; i <= n; i++){
+        cin >> tmp;
+        for (int j = tmp; j <= k; j++){
+            dp[j] += dp[j - tmp];
+        }
+    }
+    cout << dp[k] << "\n";
+    return 0;
+}
