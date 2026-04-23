4cd61abcf3ebbb3012be7ff76b3f67bcc354481f
설문혁 <150120907+Seol-Munhyeok@users.noreply.github.com>
2026-04-23 11:29:47 +0900

    [Bronze I] Title: 일곱 난쟁이, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Bronze/2309. 일곱 난쟁이/일곱 난쟁이.cc
+++ 백준/Bronze/2309. 일곱 난쟁이/일곱 난쟁이.cc
@@ -2,18 +2,28 @@
 using namespace std;
 
 int a[9];
+
+void combi(int start, vector<int> &b){
+    if (b.size() == 7){
+        int ret = accumulate(b.begin(), b.end(), 0);
+        if (ret == 100){
+            for (int i : b) cout << i << '\n';
+            exit(0);
+        }
+    }
+    for (int i = start + 1; i < 9; i++){
+        b.push_back(a[i]);
+        combi(i, b);
+        b.pop_back();
+    }
+    return;
+}
+
 int main(){
     ios::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
     for (int i = 0; i < 9; i++) cin >> a[i];
     sort(a, a + 9);
-    do {
-        int sum = 0;
-        for (int i = 0; i < 7; i++) sum += a[i];
-        if (sum == 100) {
-            for (int i = 0; i < 7; i++) cout << a[i] << '\n';
-            break;
-        }
-    } while (next_permutation(a, a + 9));
-
+    vector<int> v;
+    combi(-1, v);
     return 0;
 }
