15c46fffb34b512c00323565ea5f9209e708b73f
설문혁 <150120907+Seol-Munhyeok@users.noreply.github.com>
2026-04-23 11:20:03 +0900

    [Silver V] Title: 피카츄, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Silver/14405. 피카츄/피카츄.cc
+++ 백준/Silver/14405. 피카츄/피카츄.cc
@@ -1,17 +1,28 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-string s;
-bool flag = 0;
+string s, str;
+
 int main(){
     ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
-    cin >> s;
-    for (int i = 0; i < s.size(); i++){
-        if (i < s.size() - 1 && s.substr(i, 2) == "pi" || s.substr(i, 2) == "ka") i += 1;
-        else if (i < s.size() - 2 && s.substr(i, 3) == "chu") i += 2;
-        else { flag = 1; break; }
+    cin >> str;
+    s = "";
+    int len = 0;
+    bool ok = 1;
+    for (char ch : str){
+        s += ch;
+        if (s == "pi" || s == "ka" || s == "chu"){
+            len = 0;
+            s = "";
+        }
+        else {
+            len++;
+            if (len >= 3) { ok = 0; break; }
+        }
     }
-    if (flag) cout << "NO" << '\n';
-    else cout << "YES" << '\n';
+    if (len > 0) ok = 0;
+
+    if (ok) cout << "YES" << '\n';
+    else cout << "NO" << '\n';
     return 0;
 }
