a9896b9bf9653eb6402ace92d991742eb78ac5f1
설문혁 <150120907+Seol-Munhyeok@users.noreply.github.com>
2026-04-23 11:12:16 +0900

    [Gold IV] Title: 타임머신, Time: 8 ms, Memory: 2160 KB -BaekjoonHub

--- 백준/Gold/11657. 타임머신/타임머신.cc
+++ 백준/Gold/11657. 타임머신/타임머신.cc
@@ -1,38 +1,36 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-long long N, M, A, B, C, w[1004];
+long long N, M, A, B, C, dist[1004];
 const int INF = 1e9;
 
 int main(){
     ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
     cin >> N >> M;
-    fill(w, w + N + 1, INF);  // 가중치 배열 무한대로 초기화
-    //   (도착 노드, 비용)  출발 노드
-    vector<pair<int, int>> adj[1004];
+    fill(dist, dist + N + 1, INF);
+    vector<pair<int, int>> adj[1004];  // (노드, 비용)
     for (int i = 0; i < M; i++){
         cin >> A >> B >> C;
         adj[A - 1].push_back({B - 1, C});
     }
-    w[0] = 0;  // 0번 노드에서 출발
+    dist[0] = 0;
     bool negative_cycle = false;
     for (int i = 0; i < N; i++){
         for (int here = 0; here < N; here++){
             for (auto there : adj[here]){
                 int nxt_node = there.first;
-                int nxt_w = there.second;
-                if (w[here] != INF && w[here] + nxt_w < w[nxt_node]){
-                    w[nxt_node] = w[here] + nxt_w;
-                    // N번째 라운드에서도 값이 갱신되면, 음수 사이클이 존재한다는 뜻
-                    if (i == N - 1) negative_cycle = true;
+                int nxt_dist = there.second;
+                if (dist[here] != INF && dist[here] + nxt_dist < dist[nxt_node]){
+                    if (i == N - 1) negative_cycle = true;  // N번째 라운드에서도 값이 갱신된다면 음수 t사이클이 존재
+                    dist[nxt_node] = dist[here] + nxt_dist;
                 }
             }
         }
     }
-    if (negative_cycle) cout << -1 << "\n";
+    if (negative_cycle) cout << -1 << '\n';
     else {
         for (int i = 1; i < N; i++){
-            cout << (w[i] == INF ? -1 : w[i]) << "\n";
+            cout << (dist[i] == INF ? -1 : dist[i]) << '\n';
         }
     }
     return 0;
