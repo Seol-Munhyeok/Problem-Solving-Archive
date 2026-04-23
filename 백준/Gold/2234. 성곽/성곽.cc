aa3f4ba2359e8c65bb77709a481f566a3d46e2b6
설문혁 <150120907+Seol-Munhyeok@users.noreply.github.com>
2026-04-23 11:19:38 +0900

    [Gold III] Title: 성곽, Time: 20 ms, Memory: 2176 KB -BaekjoonHub

--- 백준/Gold/2234. 성곽/성곽.cc
+++ 백준/Gold/2234. 성곽/성곽.cc
@@ -1,21 +1,36 @@
 #include <bits/stdc++.h>
 using namespace std;
+
 const int dy[] = {0, -1, 0, 1};
 const int dx[] = {-1, 0, 1, 0};
-int visited[51][51], a[51][51], cnt, compSize[2504], N, M, mx, big;
+int N, M, a[52][52], visited[52][52], cnt, ret1, ret2, ret3;
+vector<int> v;
 
-int dfs(int y, int x, int cnt){
-    if (visited[y][x]) return 0;
-    visited[y][x] = cnt;
-    int ret = 1;
+void dfs(int y, int x, int& cnt){
+    visited[y][x] = 1;
+    cnt++;
     for (int i = 0; i < 4; i++){
-        if (!(a[y][x] & (1 << i))){
-            int ny = y + dy[i];
-            int nx = x + dx[i];
-            ret += dfs(ny, nx, cnt);
+        // 탐색순서 : 서(0001) → 북(0010) → 동(0100) → 남(1000)
+        if (a[y][x] & (1 << i)) continue;
+        int ny = y + dy[i];
+        int nx = x + dx[i];
+        if (ny < 0 || ny >= M || nx < 0 || nx >= N || visited[ny][nx]) continue;
+        dfs(ny, nx, cnt);
+    }
+    return;
+}
+
+void go(int y, int x){
+    for (int idx = 0; idx < 4; idx++){
+        if (!visited[y][x] && (a[y][x] & (1 << idx))) {
+            memset(visited, 0, sizeof(visited));
+            a[y][x] &= ~(1 << idx);
+            cnt = 0;
+            dfs(y, x, cnt);
+            v.push_back(cnt);
+            a[y][x] |= (1 << idx);
         }
     }
-    return ret;
 }
 
 int main(){
@@ -28,31 +43,22 @@ int main(){
     }
     for (int i = 0; i < M; i++){
         for (int j = 0; j < N; j++){
-            if (!visited[i][j]){
-                cnt++;
-                compSize[cnt] = dfs(i, j, cnt);
-                mx = max(mx, compSize[cnt]);
-            }
+            if (!visited[i][j]) {
+                ret1++; 
+                cnt = 0;
+                dfs(i, j, cnt);
+                ret2 = max(ret2, cnt);
+            } 
         }
     }
     for (int i = 0; i < M; i++){
         for (int j = 0; j < N; j++){
-            if (i + 1 < M){
-                int a = visited[i + 1][j];
-                int b = visited[i][j];
-                if (a != b){
-                    big = max(big, compSize[a] + compSize[b]);
-                }
-            }
-            if (j + 1 < N){
-                int a = visited[i][j + 1];
-                int b = visited[i][j];
-                if (a != b){
-                    big = max(big, compSize[a] + compSize[b]);
-                }
-            }
+            memset(visited, 0, sizeof(visited));
+            go(i, j);
         }
     }
-    cout << cnt << '\n' << mx << '\n' << big << '\n';
+    ret3 = *max_element(v.begin(), v.end());
+    cout << ret1 << '\n' << ret2 << '\n' << ret3 << '\n';
+    
     return 0;
 }
