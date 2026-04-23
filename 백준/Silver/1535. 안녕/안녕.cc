4db1aa9373212b54604660888b0e4665a7a60663
설문혁 <150120907+Seol-Munhyeok@users.noreply.github.com>
2026-04-23 11:10:56 +0900

    [Silver II] Title: 안녕, Time: 4 ms, Memory: 2036 KB -BaekjoonHub

--- 백준/Silver/1535. 안녕/안녕.cc
+++ 백준/Silver/1535. 안녕/안녕.cc
@@ -1,19 +1,23 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int N, dp[101], cost[101], happy[101];
-//dp[i] = 최대 체력 i로 얻을 수 있는 최대 기쁨
+int N, L[2002], J[2002];
+
+int knapsack(int i, int W){
+    if (i <= 0 || W <= 0) return 0;
+    if (L[i] >= W) return knapsack(i - 1, W);
+    else {
+        int not_included = knapsack(i - 1, W);
+        int included = knapsack(i - 1, W - L[i]) + J[i];
+        return max(not_included, included);
+    }
+}
 
 int main(){
     ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
     cin >> N;
-    for (int i = 0; i < N; i++) cin >> cost[i];
-    for (int i = 0; i < N; i++) cin >> happy[i];
-    for (int i = 0; i < N; i++){
-        for (int j = 100; j > cost[i]; j--){
-            dp[j] = max(dp[j], dp[j - cost[i]] + happy[i]);
-        }
-    }
-    cout << dp[100] << '\n';
+    for (int i = 1; i <= N; i++) cin >> L[i];
+    for (int i = 1; i <= N; i++) cin >> J[i];
+    cout << knapsack(N, 100) << '\n';
     return 0;
 }
