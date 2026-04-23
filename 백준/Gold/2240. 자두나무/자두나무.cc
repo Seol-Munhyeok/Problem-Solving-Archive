f88090d40b7d717319f1df4fbd7a93731e7fcbe6
설문혁 <150120907+Seol-Munhyeok@users.noreply.github.com>
2026-04-23 11:13:53 +0900

    [Gold IV] Title: 자두나무, Time: 0 ms, Memory: 2292 KB -BaekjoonHub

--- 백준/Gold/2240. 자두나무/자두나무.cc
+++ 백준/Gold/2240. 자두나무/자두나무.cc
@@ -1,21 +1,25 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int dp[1004][32][2], b[1004], T, W;
+int dp[1004][2][34], N, M, b[1004];
 
-int go(int time, int tree, int cnt){
+int go(int idx, int tree, int cnt){
+    // 기저사례
     if (cnt < 0) return -1e9;
-    if (time == T) return 0;
-    int &ret = dp[time][tree][cnt];
-    if (ret != -1) return ret;
-    return ret = max(go(time + 1, tree^1, cnt - 1), go(time + 1, tree, cnt)) + (tree == b[time] - 1);
+    if (idx == N) return 0;
+    // 메모이제이션
+    int &ret = dp[idx][tree][cnt];
+    if (~ret) return ret;  // ret != -1 을 ~ret로 구현가능
+    // 로직
+    return ret = max(go(idx + 1, tree^1, cnt - 1), go(idx + 1, tree, cnt)) + (tree == b[idx] - 1);
 }
 
 int main(){
     ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+    // 초기화
     memset(dp, -1, sizeof(dp));
-    cin >> T >> W;
-    for (int i = 0; i < T; i++) cin >> b[i];
-    cout << max(go(0, 0, W), go(0, 1, W - 1)) << '\n';
+    cin >> N >> M;
+    for (int i = 0; i < N; i++) cin >> b[i];
+    cout << max(go(0, 1, M - 1), go(0, 0, M)) << '\n';
     return 0;
 }
