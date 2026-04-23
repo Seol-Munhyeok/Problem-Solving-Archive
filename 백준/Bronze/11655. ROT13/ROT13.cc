bc0e818deb590aea4d89e58b51955c7f03661c9a
설문혁 <150120907+Seol-Munhyeok@users.noreply.github.com>
2026-04-23 11:28:57 +0900

    [Bronze I] Title: ROT13, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Bronze/11655. ROT13/ROT13.cc
+++ 백준/Bronze/11655. ROT13/ROT13.cc
@@ -5,9 +5,9 @@ string s;
 int main(){
     getline(cin, s);
     for (char ch : s){
-        if (isupper(ch)) ch = 'A' + (ch -'A' + 13) % 26;
-        else if (islower(ch)) ch = 'a' + (ch -'a' + 13) % 26;
-        cout << ch;
+        if (isspace(ch) || isdigit(ch)) cout << ch;
+        else if (isupper(ch)) printf("%c", 'A' + (ch -'A' + 13) % 26);
+        else printf("%c", 'a' + (ch -'a' + 13) % 26);
     }
     return 0;
 }
