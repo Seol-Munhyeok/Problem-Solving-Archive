61e01ad8da72fc9f6e775146b7afa1ff548bd737
설문혁 <150120907+Seol-Munhyeok@users.noreply.github.com>
2026-04-23 10:58:30 +0900

    [Gold IV] Title: 리모컨, Time: 632 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Gold/1107. 리모컨/리모컨.py
+++ 백준/Gold/1107. 리모컨/리모컨.py
@@ -1,18 +1,48 @@
 n = int(input())
 m = int(input())
-broken = set(map(int, input().split())) if m else set()
+lst = list(map(int, input().split())) if m > 0 else []
+cand = [i for i in range(10) if i not in lst]  # 사용 가능한 숫자 버튼
+length = len(str(n))
 
-def usable(x):
-    if x == 0:
-        return 0 not in broken
-    while x:
-        if x % 10 in broken:
-            return False
-        x //= 10
-    return True
+answer = int(1e9)
 
-ans = abs(n - 100)
-for x in range(1000000):  # 0 ~ 999,999
-    if usable(x):
-        ans = min(ans, len(str(x)) + abs(n - x))
-print(ans)
+def brute(arr, r, depth, value):
+    global n, answer
+    if depth == r:
+        answer = min(answer, r + abs(n - value))
+        return
+    for num in arr:
+        brute(arr, r, depth + 1, value * 10 + num)
+
+def solve(num):
+    global answer
+    # 기본값: 숫자 버튼 안 쓰고 +/-
+    answer = abs(num - 100)
+
+    # [1] 채널이 100인 경우
+    if num == 100:
+        return 0
+
+    # [2] 100 근처 채널 예외 처리
+    if num == 101 or num == 99:
+        return 1
+    if num == 102 or num == 98:
+        return 2
+    if num == 103:
+        return 3
+    
+    # [3] 모두 고장
+    if m == 10:
+        return answer
+    
+    # [4] 아무것도 안 고장
+    if m == 0:
+        return len(str(num))
+    
+    # [5] 모든 자릿수마다 경우의 수 체크
+    for r in range(1, length + 2):
+        brute(cand, r, 0, 0)
+
+    return answer
+
+print(solve(n))
