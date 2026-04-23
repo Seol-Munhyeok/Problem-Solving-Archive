b3c38d8e89b4460edd32b13fd10c5655ce90d8e0
설문혁 <150120907+Seol-Munhyeok@users.noreply.github.com>
2026-04-23 11:16:05 +0900

    [Gold V] Title: 전깃줄, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Gold/2565. 전깃줄/전깃줄.cc
+++ 백준/Gold/2565. 전깃줄/전깃줄.cc
@@ -1,21 +1,26 @@
 #include <bits/stdc++.h>
 using namespace std;
-int N, len, num;
-pair<int, int> a[104];
-int lis[104];
+int N, a[102], cnt[102], ret;
 
 int main(){
     ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
     cin >> N;
+    vector<pair<int, int>> v(N);
     for (int i = 0; i < N; i++){
-        cin >> a[i].first >> a[i].second;
+        cin >> v[i].first >> v[i].second;
     }
-    sort(a, a + N);
+    sort(v.begin(), v.end());
     for (int i = 0; i < N; i++){
-        auto lowerPos = lower_bound(lis, lis + len, a[i].second);
-        if (*lowerPos == 0) len++;
-        *lowerPos = a[i].second;
+        a[i] = v[i].second;
     }
-    cout << N - len << '\n';
+    for (int i = 0; i < N; i++){
+        int maxValue = 0;
+        for (int j = 0; j < i; j++){
+            if (a[j] < a[i] && maxValue < cnt[j]) maxValue = cnt[j];
+        }
+        cnt[i] = maxValue + 1;
+        ret = max(ret, cnt[i]);
+    }
+    cout << N - ret << '\n';
     return 0;
 }
