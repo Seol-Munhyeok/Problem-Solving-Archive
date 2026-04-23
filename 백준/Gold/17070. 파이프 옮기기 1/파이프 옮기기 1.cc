21e07da1716bc418fb5d49820d956b416e45847f
설문혁 <150120907+Seol-Munhyeok@users.noreply.github.com>
2026-04-23 11:13:28 +0900

    [Gold V] Title: 파이프 옮기기 1, Time: 0 ms, Memory: 2028 KB -BaekjoonHub

--- 백준/Gold/17070. 파이프 옮기기 1/파이프 옮기기 1.cc
+++ 백준/Gold/17070. 파이프 옮기기 1/파이프 옮기기 1.cc
@@ -1,26 +1,18 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int N, a[20][20], ret;
-const int dy[] = {0, 1, 1};
-const int dx[] = {1, 0, 1};
+int N, ret, a[20][20], dp[20][20][3];
+// dp(y좌표, x좌표, 파이프 방향(0 : 가로, 1 : 세로, 2 : 대각선))
+// 해당 지점을 끝점으로 하여 해당 방향으로 둘 수 있는 경우의 수
 
-void dfs(int y, int x, int dir){
-    if (y == N - 1 && x == N - 1){
-        ret++;
-        return;
-    }
-    for (int i = 0; i < 3; i++){
-        // 가로 -> 세로, 세로 -> 가로 붙이는 건 안됨.
-        if ((dir == 0 && i == 1) || (dir == 1 && i == 0)) continue;
-        int ny = y + dy[i];
-        int nx = x + dx[i];
-        if (ny < 0 || ny >= N || nx < 0 || nx >= N || a[ny][nx] == 1) continue;
-        // 대각선 체크
-        if (i == 2 && (a[y + 1][x] == 1 || a[y][x + 1] == 1)) continue;
-        dfs(ny, nx, i);
+bool check(int y, int x, int dir){
+    // 해당 위치에 파이프를 둘 수 있는지 판단
+    if (dir == 0 || dir == 1) return a[y][x] == 0;
+    else if (dir == 2){
+        return a[y][x] == 0 && a[y - 1][x] == 0 && a[y][x - 1] == 0;
     }
 }
+
 int main(){
     ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
     cin >> N;
@@ -29,7 +21,23 @@ int main(){
             cin >> a[i][j];
         }
     }
-    dfs(0, 1, 0);
-    cout << ret << '\n';
+    dp[0][1][0] = 1;
+    for (int i = 0; i < N; i++){
+        for (int j = 0; j < N; j++){
+            if (check(i, j + 1, 0)) dp[i][j + 1][0] += dp[i][j][0];
+            if (check(i + 1, j + 1, 2)) dp[i + 1][j + 1][2] += dp[i][j][0];
+
+            if (check(i + 1, j, 1)) dp[i + 1][j][1] += dp[i][j][1];
+            if (check(i + 1, j + 1, 2)) dp[i + 1][j + 1][2] += dp[i][j][1];
+
+            if (check(i, j + 1, 0)) dp[i][j + 1][0] += dp[i][j][2];
+            if (check(i + 1, j, 1)) dp[i + 1][j][1] += dp[i][j][2];
+            if (check(i + 1, j + 1, 2)) dp[i + 1][j + 1][2] += dp[i][j][2];
+        }
+    }
+    for (int i = 0; i < 3; i++){
+        ret += dp[N - 1][N - 1][i];
+    }
+    cout << ret <<'\n';
     return 0;
 }
