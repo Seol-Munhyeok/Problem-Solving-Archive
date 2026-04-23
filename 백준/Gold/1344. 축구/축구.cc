cbeae8a2e92c162ff7706cd3243e061de89d32ea
설문혁 <150120907+Seol-Munhyeok@users.noreply.github.com>
2026-04-23 11:09:23 +0900

    [Gold V] Title: 축구, Time: 0 ms, Memory: 2208 KB -BaekjoonHub

--- 백준/Gold/1344. 축구/축구.cc
+++ 백준/Gold/1344. 축구/축구.cc
@@ -1,9 +1,25 @@
 #include <bits/stdc++.h>
 using namespace std;
+
 const int N = 19;
 vector<int> range = {0,1,4,6,8,9,10,12,14,15,16,18};
-int C[] = {1, 18, 153, 816, 3060, 8568, 18564, 31824, 43758, 48620, 43758, 31824, 18564, 8568, 3060, 816, 153, 18, 1};
+int C[N];
+
+void initBC(){
+    // n = N - 1일 때 nCr 구하기
+    int n = N;
+    C[0] = C[1] = 1;
+    for (int i = 2; i < N; i++){
+        C[0] = C[i] = 1;
+        for(int j = i - 1; j > 0; j--){
+            C[j] += C[j - 1];
+        } 
+    }
+}
+
 int main(){
+    ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+    initBC();
     double a, b;
     cin >> a >> b;
     a /= 100;  b /= 100;
