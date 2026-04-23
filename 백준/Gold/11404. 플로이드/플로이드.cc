356786c9f908ac0f3f1b0b9fe3a7208d3cb27056
설문혁 <150120907+Seol-Munhyeok@users.noreply.github.com>
2026-04-23 11:12:21 +0900

    [Gold IV] Title: 플로이드, Time: 24 ms, Memory: 2064 KB -BaekjoonHub

--- 백준/Gold/11404. 플로이드/플로이드.cc
+++ 백준/Gold/11404. 플로이드/플로이드.cc
@@ -1,45 +1,31 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+int n, m, a, b, c, dist[104][104];
 const int INF = 1e9;
-int d[105][105];
-int n, m;
 
 int main(){
     ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
     cin >> n >> m;
-    // 처음에 전부 거리 무한대로 초기화
-    for (int i = 1; i <= n; i++){
-        fill(d[i], d[i] + 1 + n, INF);
-    }
-    // 직접 거쳐가는 경로 거리 초기화
+    fill(&dist[0][0], &dist[0][0] + 104 * 104, INF);
     while (m--){
-        int a, b, c;
         cin >> a >> b >> c;
-        d[a][b] = min(d[a][b], c);
+        a--; b--;
+        dist[a][b] = dist[a][b] ? min(dist[a][b], c) : c;
     }
-    // 자기 자신으로 가는 경우 0 초기화
-    for (int i = 1; i <= n; i++) d[i][i] = 0;
-
-    // Floyd-Warshall
-    for (int k = 1; k <= n; k++){
-        for (int i = 1; i <= n; i++){
-            for (int j = 1; j <= n; j++){
-                // d[i][j] = min(d[i][j], d[i][k]+d[k][j]);
-                // 시간 상 아래 코드가 더 빠름
-                if (d[i][j] > d[i][k] + d[k][j]){
-                    d[i][j] = d[i][k] + d[k][j];
-                }
+    for (int k = 0; k < n; k++){
+        for (int i = 0; i < n; i++){
+            for (int j = 0; j < n; j++){
+                dist[i][j] = min(dist[i][j], dist[i][k] + dist[k][j]);
             }
         }
     }
-    // 결과 출력
-    for (int i = 1; i <= n; i++){
-        for (int j = 1; j <= n; j++){
-            if (d[i][j] == INF) cout << "0 ";
-            else cout << d[i][j] << " ";
+    for (int i = 0; i < n; i++){
+        for (int j = 0; j < n; j++){
+            if (i == j) dist[i][j] = 0;
+            cout << (dist[i][j] == INF ? 0 : dist[i][j]) << " ";
         }
-        cout << "\n";
+        cout << '\n';
     }
     return 0;
 }
