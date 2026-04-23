6c3b0b65ec1337009eecd68bb835fea2000f34fb
설문혁 <150120907+Seol-Munhyeok@users.noreply.github.com>
2026-04-23 10:47:28 +0900

    [Bronze V] Title: 대전과학고등학교를 사랑하십니까?, Time: 92 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Bronze/34691. 대전과학고등학교를 사랑하십니까？/대전과학고등학교를 사랑하십니까？.py
@@ -0,0 +1,10 @@
+while True:
+    name = input()
+    if name == "end":
+        break
+    elif name == "animal":
+        print("Panthera tigris")
+    elif name == "tree":
+        print("Pinus densiflora")
+    else:
+        print("Forsythia koreana")
