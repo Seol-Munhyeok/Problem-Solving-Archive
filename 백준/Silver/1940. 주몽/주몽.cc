0423ca8328f61b1ca7660947c6733bb7bbf4aa71
설문혁 <150120907+Seol-Munhyeok@users.noreply.github.com>
2026-04-23 11:27:52 +0900

    [Silver IV] Title: 주몽, Time: 0 ms, Memory: 2080 KB -BaekjoonHub

---
+++ 백준/Silver/1940. 주몽/주몽.cc
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int N, M;
+int a[15001];
+int main(){
+    ios::sync_with_stdio(false); cin.tie(0); cout.tie(0);
+    cin >> N >> M;
+    for (int i = 0; i < N; i++)
+        cin >> a[i];
+    sort(a, a + N);
+    int start = 0, end = N - 1, cnt = 0;
+    while (start < end){
+        int sum = a[start] + a[end];
+        if (sum > M) end--;
+        else if (sum < M) start++;
+        else {
+            cnt++; start++; end--;
+        }
+    }
+    cout << cnt << '\n';
+    return 0;
+}
