b8c07ca0f582fc2cc0ea839e824e05964b04858a
설문혁 <150120907+Seol-Munhyeok@users.noreply.github.com>
2026-04-23 11:19:22 +0900

    [Gold V] Title: AC, Time: 40 ms, Memory: 8996 KB -BaekjoonHub

--- 백준/Gold/5430. AC/AC.cc
+++ 백준/Gold/5430. AC/AC.cc
@@ -1,46 +1,60 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int T, N, x;
-string P, order;
+
+int T, N;
+string str, arr, b;
+deque<string> d;
+
+deque<string> spilt(const string& input, string delimeter){
+    deque<string> result;
+    auto start = 0;
+    auto end = input.find(delimeter);
+    while (end != string::npos){
+        result.push_back(input.substr(start, end - start));
+        start = end + delimeter.size();
+        end = input.find(delimeter, start);
+    }
+    result.push_back(input.substr(start));
+    return result;
+}
+
+void print(deque<string>& d){
+    string ret = "[";
+    for (int i = 0; i < d.size(); i++){
+        ret += d[i];
+        if (i < d.size() - 1) ret += ",";
+    }
+    ret += "]";
+    cout << ret << '\n';
+}
+
 int main(){
     ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
     cin >> T;
     while (T--){
-        deque<int> D;
-        cin >> P >> N >> order;
-        x = 0;
-        for (char c : order){
-            if (c == '[' || c == ']') continue;
-            if (c >= '0' && c <= '9') x = x * 10 + c - '0';
-            else {
-                if (x > 0) D.push_back(x);
-                x = 0;
-            }
+        cin >> str >> N >> arr;
+        if (N >= 1){
+            arr = arr.substr(1, arr.size() - 2);
+            d = spilt(arr, ",");
         }
-        if (x > 0) D.push_back(x);
-        bool error = false, rev = false;
-        for (char a : P){
-            if (a == 'R') rev = !rev;
-            else {
-                if (D.empty()){
-                    error = true;
+        else d = {};
+        bool ok = true; 
+        bool filpped = false;
+        for (char ch : str){
+            if (ch == 'R') filpped = !filpped;
+            if (ch == 'D'){
+                if (d.size() && filpped) d.pop_back();
+                else if (d.size() && !filpped) d.pop_front();
+                else {
+                    ok = false;
+                    cout << "error\n"; 
                     break;
                 }
-                if (rev) D.pop_back();
-                else D.pop_front();
-            }
-        } 
-        if (error) cout << "error" << '\n';
-        else {
-            cout << "[";
-            if (rev) reverse(D.begin(), D.end());
-            for (int i = 0; i < D.size(); i++){
-                cout << D[i];
-                if (i < D.size() - 1) cout << ",";
-            }
-            cout << "]\n";
+            } 
         }
+        if (filpped) reverse(d.begin(), d.end());
+        if (ok) print(d);
     }
     return 0;
 }
