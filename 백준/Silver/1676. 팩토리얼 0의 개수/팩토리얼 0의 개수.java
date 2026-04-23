6a3fb11d61bb0859b46cb577da3fbb01a9e7aca9
설문혁 <150120907+Seol-Munhyeok@users.noreply.github.com>
2026-04-23 10:54:10 +0900

    [Silver V] Title: 팩토리얼 0의 개수, Time: 100 ms, Memory: 12868 KB -BaekjoonHub

---
+++ 백준/Silver/1676. 팩토리얼 0의 개수/팩토리얼 0의 개수.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+public class Main {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int N = sc.nextInt();
+		int count = 0;
+		while (N > 0) {
+			N /= 5;
+			count += N;
+		}
+		System.out.println(count);
+	}
+}
