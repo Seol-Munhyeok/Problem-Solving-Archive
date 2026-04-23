d451e8eef18ec72ae47015b339043ed6d5136dd6
설문혁 <150120907+Seol-Munhyeok@users.noreply.github.com>
2026-04-23 10:59:07 +0900

    [Gold IV] Title: 램프, Time: 76 ms, Memory: 34968 KB -BaekjoonHub

--- 백준/Gold/1034. 램프/램프.py
+++ 백준/Gold/1034. 램프/램프.py
@@ -4,6 +4,7 @@ n, m = map(int, input().split())
 a = [list(map(int, input())) for _ in range(n)]
 k = int(input())
 
+zero_index = []
 count = defaultdict(int)
 for i in range(n):
     temp = []
@@ -11,6 +12,29 @@ for i in range(n):
         if a[i][j] == 0:
             temp.append(j)
     count[tuple(temp)] += 1
+#print(count)
+
+max_answer = -1
+zero_count = -1
+for comb in zero_index:
+    # 각 조합에 따라 스위치 ON
+    for r in comb:
+        for i in range(n):
+            a[i][r] ^= 1
+
+    # 켜져있는 행의 수 찾기
+    answer = 0
+    for row in a:
+        if all(row):
+            answer += 1
+    if answer > max_answer:
+        max_answer = answer
+        zero_count = len(comb)
+
+    # 원상복구
+    for r in comb:
+        for i in range(n):
+            a[i][r] ^= 1
 
 sorted_count = sorted(count.items(), key=lambda x: x[1], reverse=True)
 for key, v in sorted_count:
