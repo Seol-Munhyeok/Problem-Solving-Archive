49907faada7bbfccf615bbcadef582e710aa5ead
설문혁 <150120907+Seol-Munhyeok@users.noreply.github.com>
2026-04-23 11:33:48 +0900

    [Silver V] Title: 숫자 카드, Time: 1860 ms, Memory: 113140 KB -BaekjoonHub

--- 백준/Silver/10815. 숫자 카드/숫자 카드.py
+++ 백준/Silver/10815. 숫자 카드/숫자 카드.py
@@ -2,19 +2,28 @@ import sys
 
 
 n = int(sys.stdin.readline())
-my_cards = list(map(int, sys.stdin.readline().split()))
-
-card_dict = {}
-
-for card in my_cards:
-    card_dict[card] = True
-
-
+my_cards =  sorted(list(map(int, sys.stdin.readline().split())))
 m = int(sys.stdin.readline())
-target_numbers = list(map(int, sys.stdin.readline().split()))
+target_numbers =list(map(int, sys.stdin.readline().split()))
 
-for target in target_numbers:
-    if card_dict.get(target):
-        print(1, end=' ')
-    else:
-        print(0, end=' ')
+
+def binary_search(lst, target):
+    start_index = 0
+    end_index = len(lst) - 1
+
+    while start_index <= end_index:
+        mid_index = (start_index + end_index) // 2
+
+        if lst[mid_index] == target:
+            return 1
+
+        if lst[mid_index] < target:
+            start_index = mid_index + 1
+        else:
+            end_index = mid_index - 1
+
+    return 0
+
+
+for idx in range(m):
+    print(binary_search(my_cards, target_numbers[idx]), end=' ')
