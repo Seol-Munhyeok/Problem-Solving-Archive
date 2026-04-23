50237e4d0826981b6e73a2ed6e40b75601066c59
설문혁 <150120907+Seol-Munhyeok@users.noreply.github.com>
2026-04-23 11:18:40 +0900

    [Silver V] Title: 칠무해, Time: 6508 ms, Memory: 198640 KB -BaekjoonHub

--- 백준/Silver/14729. 칠무해/칠무해.cc
+++ 백준/Silver/14729. 칠무해/칠무해.cc
@@ -1,22 +1,20 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int n;
-double temp;
-priority_queue<double> pq; 
-vector<double> v; 
-int main () {
-	cin >> n; 
-	for(int i = 0; i < n; i++){
-		cin >> temp;
-		if(pq.size() == 7){
-			pq.push(temp); pq.pop();
-		}else pq.push(temp); 
-	}
-	while(pq.size()){
-		v.push_back(pq.top()); pq.pop();
-	}
-	reverse(v.begin(), v.end()); 
-	for(double i : v) printf("%.3lf\n", i); 
-	return 0;
+int N;
+double d;
+int main(){
+    priority_queue<double, vector<double>, greater<double>> pq; 
+    cin >> N;
+    while (N--){
+        cin >> d;
+        pq.push(d);
+    }
+    cout << fixed;
+    cout.precision(3);
+    for (int i = 0; i < 7; i++){
+        cout << pq.top() << '\n';
+        pq.pop();
+    }
+    return 0;
 }
