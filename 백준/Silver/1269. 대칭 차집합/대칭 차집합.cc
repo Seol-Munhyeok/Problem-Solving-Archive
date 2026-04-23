fb0f6dc9cdf07c4db3a76578e5f51619084db771
설문혁 <150120907+Seol-Munhyeok@users.noreply.github.com>
2026-04-23 11:14:26 +0900

    [Silver IV] Title: 대칭 차집합, Time: 76 ms, Memory: 5224 KB -BaekjoonHub

--- 백준/Silver/1269. 대칭 차집합/대칭 차집합.cc
+++ 백준/Silver/1269. 대칭 차집합/대칭 차집합.cc
@@ -1,20 +1,33 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int N, M, cnt, tmp;
-map<int, int> mp;
+int N, M, cnt, x;
+vector<int> a, b;
+bool check(int target){
+    int l = 0, r = N - 1;
+    while (l <= r){
+        int mid = (l + r) / 2;
+        if (a[mid] < target) l = mid + 1;
+        else if (a[mid] > target) r = mid - 1;
+        else return true;
+    }
+    return false;
+}
+
 int main(){
     ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
     cin >> N >> M;
-    for (int i = 0; i < N; i++){
-        cin >> tmp; mp[tmp]++;
+    
+    for (int i = 0; i < N; i++) {
+        cin >> x;
+        a.push_back(x);
     }
-    for (int i = 0; i < M; i++){
-        cin >> tmp; mp[tmp]++;
+    for (int i = 0; i < M; i++) {
+        cin >> x;
+        b.push_back(x);
     }
-    for (auto it : mp){
-        if (it.second == 1) cnt++;
-    }
-    cout << cnt << '\n';
+    sort(a.begin(), a.end());
+    for (int num : b) if (check(num)) cnt++;
+    cout << N + M - (2 * cnt) << '\n';
     return 0;
 }
