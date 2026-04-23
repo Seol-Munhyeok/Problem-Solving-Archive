c8eb63acf139456c98ebb0d5681d3d58394c3a7c
설문혁 <150120907+Seol-Munhyeok@users.noreply.github.com>
2026-04-23 10:58:03 +0900

    [Silver IV] Title: 30번, Time: 2852 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/13116. 30번/30번.py
+++ 백준/Silver/13116. 30번/30번.py
@@ -1,7 +1,15 @@
+def go(a, b):
+    if a > b:
+        a, b = b, a
+    while True:
+        while b > a:
+            b //= 2
+        if b == a:
+            return 10 * a
+        while a > b:
+            a //= 2
+    
 t = int(input())
 for _ in range(t):
     a, b = map(int, input().split())
-    while a != b:
-        if a > b: a //= 2
-        else: b //= 2
-    print(10 * a)
+    print(go(a, b))
