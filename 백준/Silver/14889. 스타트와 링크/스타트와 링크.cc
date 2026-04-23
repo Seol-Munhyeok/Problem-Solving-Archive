9cfd189fbe921c61bb45525f4dbf4d7c291bffb0
설문혁 <150120907+Seol-Munhyeok@users.noreply.github.com>
2026-04-23 11:16:33 +0900

    [Silver I] Title: 스타트와 링크, Time: 236 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Silver/14889. 스타트와 링크/스타트와 링크.cc
+++ 백준/Silver/14889. 스타트와 링크/스타트와 링크.cc
@@ -1,18 +1,32 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int s[22][22], ret = 1e9, N;
+int N, cnt, a[22][22], ret = 1e9;
+vector<int> v;
 
-int go(vector<int>& a, vector<int>& b){
-    pair<int, int> ret;
-    for (int i = 0; i < N / 2; i++){
-        for (int j = 0; j < N / 2; j++){
+void go(){
+    int x = 0, y = 0;
+    for (int i = 0; i < N; i++){
+        for (int j = 0; j < N; j++){
             if (i == j) continue;
-            ret.first += s[a[i]][a[j]];
-            ret.second += s[b[i]][b[j]];
+            if (v[i] == 1 && v[j] == 1) x += a[i][j];
+            else if (v[i] == 0 && v[j] == 0) y += a[i][j];
         }
     }
-    return abs(ret.first - ret.second);
+    ret = min(ret, abs(x - y));
+}
+
+void combi(int start){
+    if (cnt == (N / 2)){
+        go();
+        return;
+    }
+    for (int i = start + 1; i < N; i++) {
+        v[i] = 1; cnt++;
+        combi(i);
+        v[i] = 0; cnt--;
+    }
+    return;
 }
 
 int main(){
@@ -20,18 +34,11 @@ int main(){
     cin >> N;
     for (int i = 0; i < N; i++){
         for (int j = 0; j < N; j++){
-            cin >> s[i][j];
+            cin >> a[i][j];
         }
     }
-    for (int i = 0; i < (1 << N); i++){
-        if (__builtin_popcount(i) != N / 2) continue;
-        vector<int> start, link;
-        for (int j = 0; j < N; j++){
-            if (i & (1 << j)) start.push_back(j);
-            else link.push_back(j);
-        }
-        ret = min(ret, go(start, link));
-    }
+    for (int i = 0; i < N; i++) v.push_back(0);
+    combi(-1);
     cout << ret << '\n';
     return 0;
 }
