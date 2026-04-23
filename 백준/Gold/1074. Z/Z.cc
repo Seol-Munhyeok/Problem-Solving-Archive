40f09747dc4fdc3031b554dcdef73375f3a030ac
설문혁 <150120907+Seol-Munhyeok@users.noreply.github.com>
2026-04-23 11:08:57 +0900

    [Gold V] Title: Z, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Gold/1074. Z/Z.cc
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int N, r, c;
+int go(int n, int r, int c){
+    if (n == 0) return 0;
+    int half = 1 << (n - 1);
+    if (r < half && c < half) return go(n - 1, r, c);
+    if (r < half && c >= half) return half * half + go(n - 1, r, c - half);
+    if (r >= half && c < half) return 2 * half * half + go(n - 1, r - half, c);
+    return 3 * half * half + go(n - 1, r - half, c - half);
+}
+
+int main(){
+    ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+    cin >> N >> r >> c;
+    cout << go(N, r, c) << "\n";
+    return 0;
+}
