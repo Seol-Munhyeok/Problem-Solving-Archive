a6562cf64a8b26b5bf143e705a1f3bd839cc9a2b
설문혁 <150120907+Seol-Munhyeok@users.noreply.github.com>
2026-04-23 11:25:20 +0900

    [Gold IV] Title: 연구소, Time: 40 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Gold/14502. 연구소/연구소.cc
+++ 백준/Gold/14502. 연구소/연구소.cc
@@ -1,57 +1,59 @@
 #include <bits/stdc++.h>
 using namespace std;
-int a[10][10], visited[10][10], n, m, ret;
-vector<pair<int, int>> virusList, wallList;
+
+int N, M, ny, nx, ret = -1;
+int a[10][10], b[10][10], visited[10][10];
 const int dy[] = {-1, 0, 1, 0};
 const int dx[] = {0, 1, 0, -1};
+vector<pair<int, int>> v;
 
 void dfs(int y, int x){
-    for (int i = 0; i < 4; i++){
-        int ny = y + dy[i];
-        int nx = x + dx[i];
-        if (ny < 0 || ny >= n || nx < 0 || nx >= m || visited[ny][nx] || a[ny][nx] == 1)
-            continue;
-        visited[ny][nx] = 1;
-        dfs(ny, nx);  
+    visited[y][x] = 1;
+    for (int dir = 0; dir < 4; dir++){
+        ny = y + dy[dir];
+        nx = x + dx[dir];
+        if (ny < 0 || ny >= N || nx < 0 || nx >= M) continue;
+        if (!visited[ny][nx] && a[ny][nx] == 0) {
+            a[ny][nx] = 1; dfs(ny, nx);
+        } 
     }
     return;
 }
 
-int solve(){
-    fill(&visited[0][0], &visited[0][0] + 10 * 10, 0);
-    for (pair<int, int> b : virusList){
-        visited[b.first][b.second] = 1;
-        dfs(b.first, b.second);
-    }
-
+int go(){
     int cnt = 0;
-    for (int i = 0; i < n; i++){
-        for (int j = 0; j < m; j++){
-            if (a[i][j] == 0 && !visited[i][j]) cnt++;
+    for (int i = 0; i < N; i++){
+        for (int j = 0; j < M; j++){
+            if (a[i][j] == 2) dfs(i, j);
+        }
+    }
+    for (int i = 0; i < N; i++){
+        for (int j = 0; j < M; j++){
+            if (a[i][j] == 0) cnt++;
         }
     }
     return cnt;
 }
 
 int main(){
-    cin >> n >> m;
-    for (int i = 0; i < n; i++){
-        for (int j = 0; j < m; j++){
+    ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+    cin >> N >> M;
+    for (int i = 0; i < N; i++){
+        for (int j = 0; j < M; j++){
             cin >> a[i][j];
-            if (a[i][j] == 2) virusList.push_back({i, j});
-            if (a[i][j] == 0) wallList.push_back({i, j});
+            if (a[i][j] == 0) v.push_back({i, j});
         }
     }
-    for (int i = 0; i < wallList.size(); i++){
+    copy(&a[0][0], &a[0][0]+100, &b[0][0]);
+    for (int i = 0; i < v.size(); i++){
         for (int j = 0; j < i; j++){
             for (int k = 0; k < j; k++){
-                a[wallList[i].first][wallList[i].second] = 1;
-                a[wallList[j].first][wallList[j].second] = 1;
-                a[wallList[k].first][wallList[k].second] = 1;
-                ret = max(ret, solve());
-                a[wallList[i].first][wallList[i].second] = 0;
-                a[wallList[j].first][wallList[j].second] = 0;
-                a[wallList[k].first][wallList[k].second] = 0;
+                memset(visited, 0, sizeof(visited));
+                a[v[i].first][v[i].second] = 1;
+                a[v[j].first][v[j].second] = 1;
+                a[v[k].first][v[k].second] = 1;
+                ret = max(ret, go());
+                copy(&b[0][0], &b[0][0]+100, &a[0][0]);
             }
         }
     }
