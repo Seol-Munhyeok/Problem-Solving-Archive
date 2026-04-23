5b34ca55fe3d2878b7fe76b4b4434042956ab8e1
설문혁 <150120907+Seol-Munhyeok@users.noreply.github.com>
2026-04-23 11:32:06 +0900

    [Gold V] Title: 소수인팰린드롬, Time: 1836 ms, Memory: 33376 KB -BaekjoonHub

--- 백준/Gold/1990. 소수인팰린드롬/소수인팰린드롬.py
+++ 백준/Gold/1990. 소수인팰린드롬/소수인팰린드롬.py
@@ -3,16 +3,17 @@ import math, sys
 n, m = map(int, sys.stdin.readline().split())
 if n % 2 == 0:
     n += 1
-
-# 팰린드롬 수의 길이가 짝수인 경우 25377352와 같다면 11, 1001, 100001 등이
-    # 모두 11의 배수이기 때문에 11 하나를 제외하고는 짝수 길이 팰린드롬은 소수가 될 수 없습니다.
-    # 이 최적화를 하면 b의 범위가 최대 10,000,000으로 제한되는 것과 같게 되므로 수행시간이 1/10 정도로 감소합니다.
 if m > 10000000:
     m = 10000000
 
 
 def is_palindrome(num):
     num_str = str(num)
+    # 팰린드롬 수의 길이가 짝수인 경우 25377352와 같다면 11, 1001, 100001 등이
+    # 모두 11의 배수이기 때문에 11 하나를 제외하고는 짝수 길이 팰린드롬은 소수가 될 수 없습니다.
+    # 이 최적화를 하면 b의 범위가 최대 10,000,000으로 제한되는 것과 같게 되므로 수행시간이 1/10 정도로 감소합니다.
+    if num != 11 and len(num_str) % 2 == 0:
+        return False
     return num_str == num_str[::-1]
 
 
@@ -25,7 +26,6 @@ def is_prime(number):
 
 
 for num in range(n, m+1, 2):
-    if num == 11 or len(str(num)) % 2 != 0:
-        if is_palindrome(num) and is_prime(num):
-            print(num)
+    if is_palindrome(num) and is_prime(num):
+        print(num)
 print(-1)
