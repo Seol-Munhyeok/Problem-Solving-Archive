bb1f58d7b12e8f85361a954e1d5a6349fcbf997b
설문혁 <150120907+Seol-Munhyeok@users.noreply.github.com>
2026-04-23 11:18:15 +0900

    [Gold IV] Title: 문자열 폭발, Time: 60 ms, Memory: 6072 KB -BaekjoonHub

--- 백준/Gold/9935. 문자열 폭발/문자열 폭발.cc
+++ 백준/Gold/9935. 문자열 폭발/문자열 폭발.cc
@@ -2,35 +2,19 @@
 using namespace std;
 
 string s, bomb, ret;
-stack<char> stk;
 
 int main(){
     ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
     cin >> s >> bomb;
+    int len = bomb.size();
     for (char ch : s){
-        stk.push(ch);
-        if (stk.size() >= bomb.size() && stk.top() == bomb[bomb.size() - 1]){
-            string ss = "";
-            for (char i : bomb){
-                ss += stk.top();
-                stk.pop();
-            }
-            reverse(ss.begin(), ss.end());
-            if (bomb != ss){
-                for (char i : ss){
-                    stk.push(i);
-                }
-                
+        ret += ch;
+        if (ret.size() >= len){
+            if (ret.substr(ret.size() - len) == bomb){
+                ret.erase(ret.size() - len, len);
             }
         }
     }
-    if (stk.size() == 0) cout << "FRULA\n";
-    else {
-        while (stk.size()){
-            ret += stk.top(); stk.pop();
-        }
-        reverse(ret.begin(), ret.end());
-        cout << ret << '\n';
-    }
+    cout << ((ret.size() == 0) ? "FRULA" : ret) << '\n';
     return 0;
 }
