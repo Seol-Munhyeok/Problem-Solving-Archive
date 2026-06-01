eb7a7cfc3570a8c8246e92611073ebfc00b55f7e
설문혁 <150120907+Seol-Munhyeok@users.noreply.github.com>
2026-06-01 21:27:58 +0900

    [level 2] Title: [1차] 캐시, Time: 23.51 ms, Memory: 114 MB -BaekjoonHub

--- 프로그래머스/2/17680. ［1차］ 캐시/［1차］ 캐시.java
+++ 프로그래머스/2/17680. ［1차］ 캐시/［1차］ 캐시.java
@@ -1,32 +1,29 @@
 import java.util.*;
-
+/**
+LRU(Least Recently Used) 총 실행시간 출력
+0 <= cacheSize <= 30, cities.length = 100_000
+도시 이름은 공백, 숫자, 특수문자 등이 없는 영문자, 대소문자 구분 안함, <= 20
+    모든 이름을 소문자로 전처리
+*/
 class Solution {
-    public int solution(int cacheSize, String[] cities) {
+    public int solution(int cacheSize, String[] cities) {   
         // 예외 처리
-        if (cacheSize == 0) return 5 * cities.length;
+        if (cacheSize == 0) return 5 * cities.length;  
         
-        // 모든 문자 소문자로 전처리
-        for (int i = 0; i < cities.length; i++) {
-            cities[i] = cities[i].toLowerCase();
-        }
-        
+        LinkedList<String> cache = new LinkedList<>();
         int answer = 0;
-        // accessOrder = true
-        // get/put으로 접근한 원소가 뒤로 이동
-        LinkedHashMap<String, Integer> cache = new LinkedHashMap<>(16, 0.75f, true);
         for (int i = 0; i < cities.length; i++) {
-            String city = cities[i];
-            if (!cache.containsKey(city)) {
+            String city = cities[i].toLowerCase();
+            if (!cache.contains(city)) {
                 answer += 5;
-                if (cache.size() == cacheSize) {
-                    String oldestKey = cache.keySet().iterator().next();
-                    cache.remove(oldestKey);  // O(1)
-                }
-            } else answer += 1;
-            
-            cache.put(city, 1);
+                if (cache.size() == cacheSize) cache.pollFirst();
+            } else {
+                answer += 1;
+                cache.remove(city);  // O(N)    
+            }
+            cache.addLast(city);
         }
-                
+         
         return answer;
     }
 }
