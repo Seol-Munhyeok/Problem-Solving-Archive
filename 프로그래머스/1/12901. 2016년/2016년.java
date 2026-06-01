cdc8cd612653abe52a740abe81010bd3a67d7225
설문혁 <150120907+Seol-Munhyeok@users.noreply.github.com>
2026-06-01 21:37:09 +0900

    [level 1] Title: 2016년, Time: 2.41 ms, Memory: 64.5 MB -BaekjoonHub

---
+++ 프로그래머스/1/12901. 2016년/2016년.java
@@ -0,0 +1,8 @@
+import java.time.*;
+class Solution {
+    public String solution(int a, int b) {
+        LocalDate date = LocalDate.of(2016, a, b);
+        DayOfWeek day = date.getDayOfWeek();
+        return day.toString().substring(0, 3);
+    }
+}
