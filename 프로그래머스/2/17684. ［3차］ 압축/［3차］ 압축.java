a02fac5d3e022f01ab971936699df7f99233e166
설문혁 <150120907+Seol-Munhyeok@users.noreply.github.com>
2026-06-08 14:51:48 +0900

    [level 2] Title: [3차] 압축, Time: 2.74 ms, Memory: 85.4 MB -BaekjoonHub

--- 프로그래머스/2/17684. ［3차］ 압축/［3차］ 압축.java
+++ 프로그래머스/2/17684. ［3차］ 압축/［3차］ 압축.java
@@ -15,15 +15,17 @@ class Solution {
         // 핵심 로직
         List<Integer> answerList = new ArrayList<>();
         int start = 0, end = 0;
-        while (end < msg.length()) {
+        while (start < msg.length()) {
+            // tmp: 사전에 없는 새 문자열 후보
             String tmp = "";
             for (end = start; end < msg.length(); end++) {
                 tmp = msg.substring(start, end + 1);
                 if (!dict.containsKey(tmp)) break;
             }
             if (!dict.containsKey(tmp)) {
-                dict.put(tmp, dict.keySet().size() + 1);
+                dict.put(tmp, dict.size() + 1);
             }
+            // msg.substring(start, end): 사전에 존재하는 가장 긴 문자열
             answerList.add(dict.get(msg.substring(start, end)));
             start = end;
         }
